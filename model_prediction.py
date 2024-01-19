5c45d75cbc15410abe71f89735244ff31c1dc793
crazyinter <2414602110@qq.com>
2024-01-19 14:59:01 +0800

    Update model_prediction.py

--- model_prediction.py
+++ model_prediction.py
@@ -18,40 +18,45 @@ test_matrix = np.array(test_matrix)
 
 result = list()
 
-model1 = load_model('100_400_all_train_model.h5')
-model2 = load_model('400_800_all_train_model.h5')
-model3 = load_model('800_1200_all_train_model.h5')
-model4 = load_model('1200_1800_all_train_model.h5')
+model1 = load_model('model_1.h5')
+model2 = load_model('model_2.h5')
+model3 = load_model('model_3.h5')
+model4 = load_model('model_4.h5')
+model5 = load_model('model_5.h5')
 
 for i in range(int(reads_number)):
     result_pre = list()
     number = 0
     for j in range(int(size2)):
         test = test_matrix[i][j]
-        if test != []:
-            if len(test)<=400:
-                model = model1
-           
-                preds = model.predict(np.reshape(test,(1,400,4)))
-                k = 400
-                number = number+400
-            elif (400<len(test)) and (len(test)<=800):
-                model = model2
-                preds = model.predict(np.reshape(test, (1, 800, 4)))
-                k = 800
-                number = number + 800
-            elif (800<len(test)) and (len(test)<=1200):
-                model = model3
-                preds = model.predict(np.reshape(test, (1, 1200, 4)))
-                k = 1200
-                number = number + 1200
-            else:
-                model = model4
-                preds = model.predict(np.reshape(test, (1, 1800, 4)))
-                k = 1800
-                number = number + 1800
-
-            result_pre.append(k*preds)
+        
+        if len(test)<=300:
+            model = model1
+            preds = model.predict(np.reshape(test,(1,300,20)))
+            k = 300
+            number = number+300
+        elif (300<len(test)) and (len(test)<=500):
+            model = model2
+            preds = model.predict(np.reshape(test, (1, 500, 20)))
+            k = 500
+            number = number + 500
+        elif (500<len(test)) and (len(test)<=1000):
+            model = model3
+            preds = model.predict(np.reshape(test, (1, 1000, 20)))
+            k = 1000
+            number = number + 1000
+        elif (1000<len(test)) and (len(test)<=2000):
+            model = model4
+            preds = model.predict(np.reshape(test, (1, 2000, 20)))
+            k = 2000
+            number = number + 2000
+         else:
+            model = model4
+            preds = model.predict(np.reshape(test, (1, 9000, 20)))
+            k = 9000
+            number = number + 9000
+             
+        result_pre.append(k*preds)
 
     result1 = np.sum(np.array(result_pre) / number)
     result.append(result1)
