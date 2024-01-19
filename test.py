0c8156cdef168526060671bfa48eca3a73266937
crazyinter <2414602110@qq.com>
2024-01-19 15:12:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -18,46 +18,19 @@ test_matrix = np.array(test_matrix)
 
 result = list()
 
-model1 = load_model('model_1.h5')
-model2 = load_model('model_2.h5')
-model3 = load_model('model_3.h5')
-model4 = load_model('model_4.h5')
-model5 = load_model('model_5.h5')
+model1 = load_model('model.h5')
 
 for i in range(int(reads_number)):
     result_pre = list()
     number = 0
     for j in range(int(size2)):
-        test = test_matrix[i][j]
         
-        if len(test)<=300:
-            model = model1
-            preds = model.predict(np.reshape(test,(1,300,20)))
-            k = 300
-            number = number+300
-        elif (300<len(test)) and (len(test)<=500):
-            model = model2
-            preds = model.predict(np.reshape(test, (1, 500, 20)))
-            k = 500
-            number = number + 500
-        elif (500<len(test)) and (len(test)<=1000):
-            model = model3
-            preds = model.predict(np.reshape(test, (1, 1000, 20)))
-            k = 1000
-            number = number + 1000
-        elif (1000<len(test)) and (len(test)<=2000):
-            model = model4
-            preds = model.predict(np.reshape(test, (1, 2000, 20)))
-            k = 2000
-            number = number + 2000
-         else:
-            model = model4
-            preds = model.predict(np.reshape(test, (1, 9000, 20)))
-            k = 9000
-            number = number + 9000
+        model = model1
+        preds = model.predict(np.reshape(test, (1, 9000, 20)))
+        k = 9000
+        number = number + 9000  
              
-        result_pre.append(k*preds)
-
+    result_pre.append(k*preds)
     result1 = np.sum(np.array(result_pre) / number)
     result.append(result1)
 np.savetxt('result.csv',result)
